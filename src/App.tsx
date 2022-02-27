0772bdd2189bc558927a5b7db4de106bf8a9f8a1
perroudon <patrick.perroudon@gmail.com>
2022-02-27 18:55:23 +0100

    life cycle of composant App - init pokemon list

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import React, {FunctionComponent, useState} from 'react';
+import React, {FunctionComponent, useState, useEffect} from 'react';
+import POKEMONS from './models/mock-pokemon';
+import Pokemon from './models/pokemon';
 
   
 const App: FunctionComponent = () => {
 
 // ajout d'un state au composant App
-const [name, setName] = useState<String>('React App');
+// init list Pokemon
+const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
+// cycle de vie du composant
+// [] empêche l'appel ultérieur au hook aprés le chargement du composant -> componentDidMount
+useEffect(() => {
+    setPokemons(POKEMONS)
+},[])
     
  return (
-  <h1>Hello, {name} !</h1>
+    <div>
+        <h1>Pokédex</h1>
+        <p>Il y a {pokemons.length} pokemons dans le pokédex</p>
+    </div>
  )
 }
   
